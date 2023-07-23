87ef53c2fda4bda63303b7473d137d1fe830e984
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-07-23 15:30:58 +0500

    finish

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,60 @@
+import { useState, ChangeEvent } from 'react'
 import style from './home.module.css'
+import { IData } from './interfaces'
+import { data } from './constants'
 const App = (): JSX.Element => {
+  const [title, setTitle] = useState<string>('')
+
+  const [arr, setArr] = useState<IData[]>(data)
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(e.target.value)
+  }
+
+  const hanleSubmit = (): void => {
+    if (!title?.length) return
+    setTitle('')
+    const newData = {
+      id: new Date().getTime(),
+      title,
+      description: 'Description',
+    }
+    setArr([...arr, newData])
+  }
+  const deleteItem = (id: number): void => {
+    const newArr = arr.filter((item) => item.id !== id)
+    setArr(newArr)
+  }
   return (
     <div className={style.todo}>
       <h1 className={style.title}>Todo List</h1>
+      <div className={style.box}>
+        <input
+          className={style.input}
+          value={title}
+          onChange={changeHandler}
+          placeholder="Enter todo"
+          type="text"
+        />
+        <button className={style.button} onClick={hanleSubmit}>
+          Add Todo
+        </button>
+      </div>
+      <div className={style.card}>
+        {arr.map((item) => (
+          <div key={item.id} className={style.cardItem}>
+            <p className={style.cardTitle}>{item.title}</p>
+            <div>
+              <button
+                onClick={() => deleteItem(item.id)}
+                className={style.delButton}
+              >
+                Del
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ vite-project/src/constants/index.ts
@@ -0,0 +1,7 @@
+import { IData } from '../interfaces'
+
+export const data: IData[] = [
+  { id: 1, title: 'Omar', description: 'Description 1' },
+  { id: 2, title: 'Osmas', description: 'Description 2' },
+  { id: 3, title: 'Abdulloh', description: 'Description 3' },
+]
